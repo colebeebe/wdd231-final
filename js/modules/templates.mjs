36d39c10820e5e0e03be95a4872e1ed61db5f374
colebeebe <bee14003@byui.edu>
2026-02-28 15:07:04 -0500

    recipe.html now populates dynamically

--- js/modules/templates.mjs
+++ js/modules/templates.mjs
@@ -6,12 +6,23 @@
 // inserted into the html
 export function recipeCardTemplate(data) {
     // Get the recipe's image. It must be found inside images/recipes/
-    const image = 'images/recipes/' + data.image;
+    // We also check to make sure that the data exists.
+    let image = 'https://picsum.photos/100/100';
+    if (data.image) {
+        image = 'images/recipes/' + data.image;
+    }
+
+    // Just like the photo, we want to check that the alt info exists
+    // This is the ternary operator. It's basically saying 'check what's
+    // before the question mark. If true, return the first thing, and if
+    // false return the second.' In other words, it's the same thing as
+    // what we did for the image but in one line
+    const alt = data.image_alt ? data.image_alt : 'recipe photo';
 
     return `
         <div class="preview">
             <section class="info">
-                <img src="${image}" />
+                <img src="${image}" alt="${alt}" />
                 <h2>${data.recipe_name}</h2>
             </section>
             <div class="btn-container">
@@ -19,5 +30,72 @@ export function recipeCardTemplate(data) {
                 <a href="recipe.html" class="btn btn-green view">View</a>
             </div>
         </div>
+    `;
+}
+
+function ingredientsTemplate(data) {
+    return data.map(recipe => `
+        ${recipe.name ? `<h2 class="recipe-name">${recipe.name}</h2>` : ``}
+        <h3>Ingredients:</h3>
+        <ul>
+            ${recipe.ingredients.map(i => `
+                <li>
+                    ${i.amount ? i.amount : ``}
+                    ${i.unit ? i.unit : ``}
+                    ${i.name}${i.notes ? `, ${i.notes}` : ``}
+                </li>`).join('')}
+        </ul>
+    `).join('');
+}
+
+function stepsTemplate(data) {
+    return data.map(recipe => `
+        ${recipe.name ? `<h2 class="recipe-name">${recipe.name}</h2>` : ``}
+        <h3>Steps:</h3>
+        <ol>
+            ${recipe.steps.map(s => `
+                <li>${s}</li>
+            `).join('')}
+        </ol>
+    `).join('');
+}
+
+// The function to fill out the recipe page
+// Again, here 'data' represents a single recipe that will be displayed on the
+// page, rather than the entire list of recipes
+export function recipePageTemplate(data) {
+    // We also want to display the image dynamically, so we get it here
+    // Again, we check if the data exists.
+    let image = 'https://picsum.photos/800/400';
+    if (data.image) {
+        image = 'images/recipes/' + data.image;
+    }
+
+    // We also need the alt. Look at this line in recipeCardTemplate() for
+    // more information on what's going on
+    const alt = data.image_alt ? data.image_alt : 'recipe photo';
+
+    return `
+        <section class="title-info">
+            <h1>${data.recipe_name}</h1>
+            <h2>Author: ${data.author}</h2>
+        </section>
+        <hr />
+        <img src="${image}" alt="${alt}" />
+        <section class="prep-info">
+            ${data.servings ? `<p class="servings">Servings: ${data.servings}</p>`: ``}
+            ${data.yields ? `<p class="yields">Yields: ${data.yields}</p>`: ``}
+            ${data.prep_time ? `<p class="prep-time">Prep time: ${data.prep_time}</p>`: ``}
+            ${data.cook_time ? `<p class="cook-time">Cook time: ${data.cook_time}</p>`: ``}
+            ${data.total_time ? `<p class="total-time">Total time: ${data.total_time}</p>`: ``}
+        </section>
+        <hr />
+        <section class="ingredients">
+            ${ingredientsTemplate(data.subrecipes)}
+        </section>
+        <hr />
+        <section class="steps">
+            ${stepsTemplate(data.subrecipes)}
+        </section>
     `;
 }
--- js/recipe.js
+++ js/recipe.js
@@ -1,13 +1,22 @@
 // recipe.js
 
 import { getJson } from "./modules/utilities.mjs";
+import { recipePageTemplate } from "./modules/templates.mjs";
 // Remember that we import we have to include module in the <script> element
 
 // JavaScript doesn't really like to work with awaits outside of a function,
 // so we wrap everything that needs to happen in another function, usually
 // called init()
 async function init() {
-    
+    // Remember, we had to make getJson asynchronous, so we need to await it
+    const data = await getJson('./recipes.json');
+    // Get the <main> element
+    const main = document.querySelector('main');
+    // Build the HTML for the page
+    const html = recipePageTemplate(data[1]);
+    // Insert the HTML. This is non-destructive, so anything that already
+    // exists won't get overwritten
+    main.insertAdjacentHTML('beforeend', html);
 }
 
 // This is the actual call to our initialization function. Without this, it
