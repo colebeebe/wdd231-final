c82759b914e0853e3b37cce93343e1e776be87f0
colebeebe <bee14003@byui.edu>
2026-03-14 13:44:26 -0400

    Made good progress with favorites

--- js/favorites.js
+++ js/favorites.js
@@ -1,17 +1,24 @@
 // favorites.js
 
-import { getJson } from './modules/utilities.mjs';
-import { recipeCardTemplate } from "./modules/templates.mjs";
+import { getJson, addCards } from './modules/utilities.mjs';
 // Remember that if we use modules, we have to set type to module in the
 // <script element in the HTML
+
+let favorites = JSON.parse(localStorage.getItem('favorites'));
+if (!favorites) favorites = [];
 
 // The initializing function. We use this since JavaScript doesn't like
 // awaiting things outside of asynchronous functions
 async function init() {
     const data = await getJson('./recipes.json');
-    const content = document.querySelector('.content');
-    const html = data.map(recipeCardTemplate).join('');
-    content.insertAdjacentHTML('beforeend', html);
+    data.forEach(r => {
+        r.favorite = false;
+        if (favorites.find(f => f.name === r.recipe_name && f.author === r.author)) {
+            r.favorite = true;
+        }
+    });
+    const favs = data.filter(r => r.favorite);
+    addCards(favs);
 }
 
 init();
--- js/modules/utilities.mjs
+++ js/modules/utilities.mjs
@@ -1,5 +1,7 @@
 // utilities.mjs
 
+import { recipeCardTemplate } from "./templates.mjs";
+
 // This is pretty closely lifted from the nps project
 export async function getJson(url) {
     // The data variable is declared here because we're going to return it,
@@ -17,4 +19,61 @@ export async function getJson(url) {
         data = await response.json();
     } else throw new Error("response not ok.");
     return data;
+}
+
+// Even though this is getting called by an async function, it does not need
+// to be asynchronous itself. This is because we never have to await anything
+// on the inside; by the time the data has reached this point we have it
+// loaded in memory, so we should have full access to it.
+// Here, 'data' represents the JSON object that we pulled from file, with all
+// recipes (rather than just a single recipe)
+export function addCards(data) {
+    // We get this element because that's where we've designated that the
+    // cards will be added
+    const content = document.querySelector(".content");
+    // There's a lot going on this next line. At the end of the day, map() and
+    // forEach() do roughly the same thing - that is, iterate through all
+    // objects in an array (or List, or however you want to call it). The
+    // difference is that map() returns a value whereas forEach() does not. In
+    // this case, we are creating one string that represents the html for all
+    // of the cards, so we use map() to return that. Normally, you might be 
+    // accustomed to seeing something like `data.map(object => object.name)`
+    // but because recipeCardTemplate() expects two parameters, JavaScript
+    // is able to infer that we want to pass each object in `data` into the
+    // recipeCardTemplate() function call. After all of that, we need to use
+    // `join('')`, which just tells map() to join all elements with nothing
+    // (the default is commas).
+    const html = data.map(recipeCardTemplate).join('');
+    // After we've created our html, we can simply append it to the page. In
+    // this case we use insertAdjacentHTML(), which is non-destructive (it
+    // doesn't destroy everything that already exists inside the element)
+    content.insertAdjacentHTML('beforeend', html);
+
+    content.addEventListener('click', function(e) {
+        if (e.target.classList.contains('favorite')) {
+            const id = e.target.dataset.id;
+            const item = data[id]; // the specific recipe object
+
+            if (item.favorite) {
+                e.target.textContent = 'Favorite';
+                favorites = favorites.filter(f => !(f.name === item.recipe_name && f.author === item.author));
+                item.favorite = false;
+            } else {
+                e.target.textContent = 'Unfavorite';
+                const recipe = { name: item.recipe_name, author: item.author };
+                favorites.push(recipe);
+                item.favorite = true;
+            }
+
+            const marker = e.target.closest('.preview').querySelector('.favorite-marker');
+            if (marker) marker.classList.toggle('hidden');
+
+            updateFavorites();
+        }
+    });
+}
+
+function updateFavorites() {
+    if (!favorites) return;
+    localStorage.setItem('favorites', JSON.stringify(favorites));
 }
